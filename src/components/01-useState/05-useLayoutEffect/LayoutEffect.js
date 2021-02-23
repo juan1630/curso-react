97e0d75e9ad0954c30d850625730389cbe8ef088
Juan Patrón <josejuanpatron1630@gmail.com>
2021-02-22 23:48:30 -0600

    Se agrego el use layoutEffect

---
+++ src/components/01-useState/05-useLayoutEffect/LayoutEffect.js
@@ -0,0 +1,53 @@
+import React, {useLayoutEffect, useRef, useState} from 'react';
+import { useCounter } from '../../../hooks/useCounter';
+import { useFetch } from '../../../hooks/useFetch';
+import '../02-useEffect/useEffect.css';
+import './layout.css';
+
+export const Layout = () => {
+
+    const { counter, increment } = useCounter(1);
+
+
+   const {  data } = useFetch(  `https://www.breakingbadapi.com/api/quotes/${counter}`);
+   const pTag = useRef();
+
+    const {  quote } = !!data && data[0];
+
+    const [boxSize, setboxSize] = useState({});
+
+    useLayoutEffect(() => {
+       setboxSize(  pTag.current.getBoundingClientRect() );
+    }, [quote])
+
+
+    return ( 
+    <div>
+        <h1> Breaking Bad Quotes!!!!  </h1> 
+        <hr/>
+            
+                <blockquote  className="blockquote text-right  ml-3 " >
+                <p className="ml-3"
+                    ref={  pTag}
+                > 
+                    {  quote}
+                </p>
+
+                <pre> {  JSON.stringify( boxSize,null,  3 ) }  </pre>
+
+            </blockquote>
+            
+        
+
+        <button className="btn btn-primary"  onClick={ increment } > 
+            Más frases 
+        
+        </button>
+           
+
+    </div>
+    )
+}
+
+
+//
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RealExample } from './components/01-useState/04-useRef/RealExample';
+// import { RealExample } from './components/01-useState/04-useRef/RealExample';
 // import { FocusScreen } from './components/01-useState/04-useRef/FocusScreen';
 // import { MultiplaCustomHooks } from './components/01-useState/examples/MultiplaCustomHooks';
 // import { FormWithCustomHook } from './components/01-useState/02-useEffect/FormWithCustomHook';
@@ -8,8 +8,10 @@ import { RealExample } from './components/01-useState/04-useRef/RealExample';
 // import { HookApp } from './hooksApp';
 // import { CounterApp } from './components/01-useState/counterApp'
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { Layout } from './components/01-useState/05-useLayoutEffect/LayoutEffect';
 
 
-ReactDOM.render( < RealExample / > ,
+
+ReactDOM.render( < Layout /> ,
     document.getElementById('root')
 );
